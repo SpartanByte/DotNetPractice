f269a2b186a7c4296ead3541d7ccd206a6721c48
Brian Wardwell <briwar10@outlook.com>
2021-03-09 12:50:58 -0600

    added FQN example and Directory view

---
+++ ConsoleAppSimpleDirectoryView/ConsoleAppSimpleDirectoryView/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main()
+    {
+        //store folders and files inside an array of strings
+        string[] files = Directory.GetFiles(@"c:\users");
+        string[] folders = Directory.GetDirectories(@"c:\users");
+
+        foreach(string f in files)
+        {
+            Console.WriteLine("File {0} is located in {1}.", Path.GetFileName(f), Path.GetDirectoryName(f));
+        }
+
+        foreach(string f in folders)
+        {
+            Console.WriteLine("The directory is {0}.", f.ToString());
+        }
+    }
+}
