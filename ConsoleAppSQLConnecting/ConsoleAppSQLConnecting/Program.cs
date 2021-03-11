a1c3e0a06b6b9b1affcbc89f3150a65d83d99559
Brian Wardwell <briwar10@outlook.com>
2021-03-11 16:35:24 -0600

    added SQL Connection example

---
+++ ConsoleAppSQLConnecting/ConsoleAppSQLConnecting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+// add need namespaces
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=LAPTOP-T1POA9KQ\\SQLEXPRESS;Initial Catalog=sample;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        // select all data from People table
+        string command = "Select * from dbo.People";
+        // low level resource that needs to be desposed of properly
+        using (SqlConnection conn = new SqlConnection(connectionString)) 
+        {
+            try //no guarantee connection will be successful
+            {
+                conn.Open();
+                SqlCommand sqlComm = new SqlCommand(command, conn);
+                using (SqlDataReader reader = sqlComm.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine("{0}: {1} {2}", reader[0], reader[1], reader[2]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
