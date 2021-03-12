9eba5cff3e9dcf4364645510a318d2aeefefe36c
Brian Wardwell <briwar10@outlook.com>
2021-03-11 19:55:25 -0600

    added LINQ generic list search and LINQ queries examples

---
+++ ConsoleAppLINQGenericListSearch/ConsoleAppLINQGenericListSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;// for generic lists
+using System.Linq; //for doing searches
+
+class Program
+{
+    static void Main()
+    {
+        List<double> myValues = new List<double>();
+        Console.WriteLine("Enter Values:");
+        string s = Console.ReadLine();
+        
+        while(s != "Exit")
+        {
+            myValues.Add(double.Parse(s));
+            s = Console.ReadLine();
+        }
+
+        //LINQ comes in to enable search features
+        var values = from num in myValues where (num >= 0 && num <= 10) select num;
+        Console.WriteLine("Matching Values:");
+        // display values that match the given condition
+        foreach(var d in values)
+        {
+            Console.Write(d + ", ");
+        }
+        // Start a new line
+        Console.WriteLine();
+    }
+}
---
+++ ConsoleAppLambdaExpLINQQueries/ConsoleAppLambdaExpLINQQueries/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;//Ienumerable access 
+using System.Linq; //for queries in C#
+
+class Program
+{
+    static void Main()
+    {
+        //make array of values
+        int[] values = new int[] { 3, 5, 2, 8, 6, 3, 4 };
+        // find sum of values where each number is more than 5
+        // 1. scan list, comparing, then sum
+        // 3 > 5? no (do not include this in the sum)
+
+        double compareAgainst = double.Parse(Console.ReadLine());
+        Console.WriteLine(values.Where(x => x > compareAgainst).Sum());
+
+        //make a list of values
+        IEnumerable<int> lessThanList = values.TakeWhile(x => x < compareAgainst);
+        foreach(var item in lessThanList)
+        {
+            Console.Write(item + ", ");
+        }
+
+    }
+}
