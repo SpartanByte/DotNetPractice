094fbb8adfd6f3df68a87fc031685e81b3225d2a
Brian Wardwell <briwar10@outlook.com>
2021-03-12 12:40:31 -0600

    added WPF simple project, console app for threading

---
+++ ConsoleAppTheadingAndAsynchrony/ConsoleAppTheadingAndAsynchrony/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Worker()
+    {
+        //for simulating work
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+            //display i on the screen
+            if(i == 5)
+            {
+                Console.WriteLine("Falling asleep at " + DateTime.Now);
+                Thread.Sleep(5000); //Muliseconds
+                Console.WriteLine("Awaking at " + DateTime.Now);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        //new thread must be said in such a way that it starts the worker method
+        //worker is static so you don't need anything in front of "Worker"
+        Thread newThread = new Thread(new ThreadStart(Worker));
+        newThread.Name = "Hard Worker Thread!";
+        newThread.Start();
+        //suspends the current thread (Main starts the process)
+        newThread.Join();
+        Console.WriteLine("We are back in main");
+    }
+}
